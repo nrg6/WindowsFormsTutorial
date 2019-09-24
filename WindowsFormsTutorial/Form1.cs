203bfb2f6824b120375d03d9588d7c42915ca20b
Norm Griffiths <nrg6472@hotmail.com>
2019-09-24 13:50:37 +0100

    code for button and textbox.

--- WindowsFormsTutorial/Form1.cs
+++ WindowsFormsTutorial/Form1.cs
@@ -12,9 +12,16 @@ namespace WindowsFormsTutorial
 {
     public partial class Form1 : Form
     {
+        int counter = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = counter.ToString();
+            counter++;
         }
     }
 }
